acf737ab0d22a49d19b2a5de1af16db0945fd096
Anthony Grebe <agrebe@mit.edu>
2023-02-14 17:19:37 -0600

    Updated nn->pp 3-point routines to use precomputed tensor

--- run_nnpp_3pt.h
+++ run_nnpp_3pt.h
@@ -1,13 +1,14 @@
 #include "spin_mat.h"
 #include "gamma_container.h"
 #include "color_tensor.h"
+#include "run_sigma_4pt.h"
 
-void run_nnpp_3pt(SpinMat* wall_prop,      // wall prop at source
-                  SpinMat* point_prop,     // point prop at sink
-                  Vcomplex * corr,         // 3-point correlator
-                  int gamma_index,         // index of gamma to insert
-                  int block_size_sparsen,  // sparsening factor at operator
-                  int nt, int nx,          // size of lattice
-                  int tm,                  // source time
-                  int sep,                 // sink - source time
-                  int xc, int yc, int zc); // sink spatial coords
+void run_nnpp_3pt(Vcomplex * T,             // precomputed tensor
+                  SpinMat * wall_prop,      // source propagator
+                  Vcomplex * corr,          // 3-point correlator
+                  int nt, int nx,           // size of lattice
+                  int sep,                  // sink - source time
+                  int tm,                   // source time
+                  int t,                    // operator - source time
+                  int num_currents,         // {SS, PP, VV, AA, TT}
+                  int xc, int yc, int zc);  // sink coordinates
